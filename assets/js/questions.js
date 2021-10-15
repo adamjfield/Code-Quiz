cf7db8d950cd7b6454f179c3bb0b8d4ed55e76e2
Adam Field <adamjfield@gmail.com>
2021-10-15 17:02:30 -0500

    add questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -4,7 +4,7 @@ var btnElement = document.querySelector("#play");
 var quesContain = document.querySelector(".questionContainer");
 var titleElement = document.querySelector("#title");
 var olElement = document.querySelector("#questions");
-var finishPage = document.querySelector (".finish-page");
+var finishPage = document.querySelector(".finish-page");
 var finalScore = document.querySelector("#score");
 var errorMsg = document.querySelector("#errorMsg");
 var inputInitials = document.querySelector("#inputInitials");
@@ -14,3 +14,46 @@ var lastPgEle = document.querySelector(".last-page");
 var scoreAndInt = document.querySelector("#staticEmail");
 var homePage = document.querySelector("#home-page");
 
+// questions
+var questions = [
+  {
+    title: "Where is the JavaScript placed inside an HTML document or page?",
+    choices: [
+      "In the <footer> section",
+      "In the <title> section",
+      "In the <meta> section",
+      "In the <body> and <head> sections",
+    ],
+    answer: "In the <footer> section",
+  },
+  {
+    title:
+      "In JavaScript, what is a block of code called that is used to perform a specific task?",
+    choices: ["Declaration", "Function", "Variable", "String"],
+    answer: "Function",
+  },
+  {
+    title:
+      "What is the name of the statement that is used to exit or end a loop?",
+    choices: [
+      "Break statement",
+      "Falter statement",
+      "Conditional statement",
+      "Close statement",
+    ],
+    answer: "Conditional statement",
+  },
+  {
+    title:
+      "What is the element used – and hidden – in code that explains things and makes the content more readable?",
+    choices: ["Comparisons", "Comparisons", "Notes", "Comments"],
+
+    answer: "Notes",
+  },
+  {
+    title:
+      "In JavaScript, what element is used to store multiple values in a single variable?",
+    choices: ["Variables", "Functions", "Strings", "Arrays"],
+    answer: "Arrays",
+  },
+];
