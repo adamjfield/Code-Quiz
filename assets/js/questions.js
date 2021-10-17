61742bc99dee8270c503b108a9734676321d7b2b
Adam Field <adamjfield@gmail.com>
2021-10-17 14:20:50 -0500

    render questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -66,4 +66,46 @@ function displayQuestions() {
   var holdq1Choice2 = questions[i].choices[1];
   var holdq1Choice3 = questions[i].choices[2];
   var holdq1Choice4 = questions[i].choices[3];
+
+  olElement.innerHTML = "";
+
+  var liTag1 = document.createElement("li");
+  liTag1.setAttribute("class", "all_li");
+  var btn = document.createElement("button");
+  btn.setAttribute("class", "btn");
+  btn.textContent = holdq1Choice1;
+  liTag1.appendChild(btn);
+  olElement.appendChild(liTag1);
+  quesContain.appendChild(olElement);
+
+  var liTag2 = document.createElement("li");
+  liTag2.setAttribute("class", "all_li");
+  var btn2 = document.createElement("button");
+  btn2.setAttribute("class", "btn");
+  btn2.textContent = holdq1Choice2;
+  liTag2.appendChild(btn2);
+  olElement.appendChild(liTag2);
+  quesContain.appendChild(olElement);
+
+  var liTag3 = document.createElement("li");
+  liTag3.setAttribute("class", "all_li");
+  var btn3 = document.createElement("button");
+  btn3.setAttribute("class", "btn");
+  btn3.textContent = holdq1Choice3;
+  liTag3.appendChild(btn3);
+  olElement.appendChild(liTag3);
+  quesContain.appendChild(olElement);
+
+  var liTag4 = document.createElement("li");
+  liTag4.setAttribute("class", "all_li");
+  var btn4 = document.createElement("button");
+  btn4.setAttribute("class", "btn");
+  btn4.textContent = holdq1Choice4;
+  liTag4.appendChild(btn4);
+  olElement.appendChild(liTag4);
+  quesContain.appendChild(olElement);
+  var allBtnEl = document.querySelectorAll(".btn");
+  allBtnEl.forEach(function (event) {
+    event.addEventListener("click", onclickHandler);
+  });
 }
